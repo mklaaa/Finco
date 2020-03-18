ba05eb11a395efb75e65707e45cb64ce3014870a
mklaaa <mk.la@live.com>
2020-03-17 23:19:39 -0300

    First batch of tables

--- flo.py
+++ flo.py
@@ -10,15 +10,39 @@ def flo():
     conn=connection('open')
     cur=conn.cursor()
 
-    #First table
-    """
-    cur.execute('SELECT VERSION ();')
-    rec=cur.fetchone()
-    print('You are connected to: ',rec)
-    """
+    #Salary by time
+    df=pd.read_sql("""
+        SELECT DATE,SUM(VALUE) AS VALUE FROM TABFLOW WHERE TYPE='Salario' GROUP BY DATE;
+        """,conn)
+    print(tabulate(df,headers='keys',tablefmt='psql'),'\n\n')
 
-    df=pd.read_sql('SELECT * FROM TABFLOW LIMIT 5;',conn)
-    print(tabulate(df,headers='keys',tablefmt='psql'))
+    #Income and expense by time
+    df=pd.read_sql("""
+        SELECT DATE,CLASS,SUM(VALUE) AS VALUE FROM TABFLOW GROUP BY DATE, CLASS;
+        """,conn)
+    print(tabulate(df,headers='keys',tablefmt='psql'),'\n\n')
+
+    #Net profit and eff by time
+    """
+    df=pd.read_sql(""
+        WITH
+            TNET_RESULT AS (
+                SELECT DATE,
+                    SUM(CASE WHEN CLASS='Expense' THEN VALUE*(-1)
+                        ELSE VALUE
+                    END) AS NET_RESULT
+                FROM TABFLOW GROUP BY DATE),
+
+            TINCOME AS (
+                SELECT DATE,SUM(VALUE) AS INCOME FROM TABFLOW WHERE TYPE='Salario' GROUP BY DATE)
+
+            SELECT DATE,NET_RESULT FROM TNET_RESULT
+            UNION ALL
+            SELECT TINCOME.DATE,TINCOME.INCOME FROM TNET_RESULT
+            JOIN TNET_RESULT TINCOME ON TNET_RESULT.DATE = TINCOME.DATE;
+        "",conn)
+    print(tabulate(df,headers='keys',tablefmt='psql'),'\n\n')
+    """
 
     connection('close')
 
